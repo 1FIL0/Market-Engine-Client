07c97ba8c901ac1136dc740823e5d22a9c7f64d6
1FIL0 <serglinux2@gmail.com>
2025-08-29 21:49:48 +0100

    add windows cmake support

--- cpp/tradeup_engine/build/make_win64.sh
+++ cpp/tradeup_engine/build/make_win64.sh
@@ -1,6 +1,6 @@
 rm ../src/gpu_kernel_embedded/*
-cmake -S . -B build_win64 -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=g++
+cmake -S . -B build_win64 -G "MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++
 mkdir -p build_win64
 cd build_win64
-make -j12
+mingw32-make -j12
 cd ..
